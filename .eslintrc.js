f50fa8bf529c861a4a5f801377eb9d04301de4ea
crazy23lt <2394630102@qq.com>
2020-10-23 20:56:08 +0800

    102301

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-console": ["warn", { allow: ["warn"] }],
+    "no-empty": ["error", { allowEmptyCatch: true }],
+  },
+};
